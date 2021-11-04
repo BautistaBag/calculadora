e5bcc28aaaeb4e95b205cd9a81abb3c90f392966
Bautista Bagnarol <81656547+BautistaBag@users.noreply.github.com>
2021-11-04 15:51:10 -0300

    Create funcion.js

---
+++ funcion.js
@@ -0,0 +1,14 @@
+function sumar(){
+    //tomo los id de operandoA y operandoB
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    //parseInt convierte los valores a un numero. Si no uso la funcion parseInt se me 
+    //conctatenarian ambos numeros y me mostraria por pantalla ambos numeros pegados(juntos) 
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado)){
+        resultado = 'La operacion no contiene numeros';
+    }
+    document.getElementById('resultado').innerHTML = 'Resultado: ' + resultado;
+    console.log('Resultado: ' + resultado);
+}
